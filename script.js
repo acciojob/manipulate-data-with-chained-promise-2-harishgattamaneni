b63511a5bdc359363087890c5064731212096024
harishgattamaneni <103264547+harishgattamaneni@users.noreply.github.com>
2025-09-16 07:48:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+function myPromise(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(() => {
+            resolve([1,2,3,4])
+        }, 3000);
+	})
+}
+
+myPromise().then((data)=>{
+    const evenNumbers=data.filter((Element,i)=>{
+        if(Element%2==1){
+            return true
+        }
+    })
+    setTimeout(() => {
+        document.getElementById('output').innerText = evenNumbers.join(',');
+    }, 1000);
+    return evenNumbers
+}).then((data)=>{
+    const multiplied=data.map((Element,i)=>{
+        return Element*2
+    })
+    setTimeout(() => {
+        document.getElementById('output').innerText = multiplied.join(',');
+    }, 2000);
+})
